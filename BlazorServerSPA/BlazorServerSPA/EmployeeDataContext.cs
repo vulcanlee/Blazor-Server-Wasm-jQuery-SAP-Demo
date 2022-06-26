5ba969f15f64a7b8b0e97380d527c14e88e486dd
Vulcan <vulcan.lee@gmail.com>
2022-06-26 21:12:37 +0800

    使用 ASP.NET Core 6 + Blazor Server 設計出 CRUD 的 SPA 網站服務

---
+++ BlazorServerSPA/BlazorServerSPA/EmployeeDataContext.cs
@@ -0,0 +1,14 @@
+﻿using BlazorServerSPA.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorServerSPA
+{
+   public class EmployeeDataContext : DbContext
+   {
+      public EmployeeDataContext(DbContextOptions<EmployeeDataContext> options) : base(options)
+      {
+      }
+
+      public DbSet<Employee> Employee { get; set; }
+   }
+}
