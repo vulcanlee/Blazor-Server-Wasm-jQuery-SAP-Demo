c72eb0faa6b8cdc63cef5221032002703e1217e3
Vulcan <vulcan.lee@gmail.com>
2022-06-26 21:12:04 +0800

    使用 ASP.NET Core 6 + jQuery 設計出 CRUD 的 SPA 網站服務

---
+++ jQuerySPA/jQuerySPA/EmployeeDataContext.cs
@@ -0,0 +1,14 @@
+﻿using jQuerySPA.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace jQuerySPA
+{
+   public class EmployeeDataContext : DbContext
+   {
+      public EmployeeDataContext(DbContextOptions<EmployeeDataContext> options) : base(options)
+      {
+      }
+
+      public DbSet<Employee> Employee { get; set; }
+   }
+}
---
+++ jQuerySPA/jQuerySPA/Program.cs
@@ -0,0 +1,59 @@
+using jQuerySPA;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+#region 註冊 DbContext 服務
+string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<EmployeeDataContext>(options =>
+        options.UseSqlServer(connectionString));
+
+#endregion
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+#region 產生資料庫預設紀錄
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<EmployeeDataContext>();
+        DbInitializer.Initialize(context);
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while seeding the database.");
+    }
+}
+#endregion
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+#region 加入 MVC 路由宣告
+app.MapControllers();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+#endregion
+
+app.MapRazorPages();
+
+app.Run();
