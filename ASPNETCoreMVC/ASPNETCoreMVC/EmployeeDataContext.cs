fd10df39da11e751c44d0352b238ca53df5a7665
Vulcan <vulcan.lee@gmail.com>
2022-06-27 23:47:35 +0800

    使用 ASP.NET Core MVC 實作 CRUD

---
+++ ASPNETCoreMVC/ASPNETCoreMVC/EmployeeDataContext.cs
@@ -0,0 +1,14 @@
+﻿using ASPNETCoreMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPNETCoreMVC
+{
+   public class EmployeeDataContext : DbContext
+   {
+      public EmployeeDataContext(DbContextOptions<EmployeeDataContext> options) : base(options)
+      {
+      }
+
+      public DbSet<Employee> Employee { get; set; }
+   }
+}
